a3ada79bbc92df5d6f4cd53b25ab2ff7f1d726cb
Bobby Kiaie <bobbykiaie@gmail.com>
2023-12-06 17:16:36 -0800

    -Add data service

--- CitySights App/ContentView.swift
+++ CitySights App/ContentView.swift
@@ -10,17 +10,27 @@ import SwiftUI
 struct ContentView: View {
     
     @State var query: String = ""
+    var service = DataService()
     
     var body: some View {
         HStack {
             TextField("What are you looking for ", text: $query).textFieldStyle(.roundedBorder)
             Button {
-                
+                let apiKey = Bundle.main.infoDictionary?["API_KEY"] as? String ?? "No Key"
+                print(apiKey)
             } label: {
                 Text("Go")
+                    .padding(.horizontal)
+                    .padding(.vertical, 10)
+                    .background(.blue)
+                    .foregroundStyle(.white)
+                    .clipShape(.rect(cornerRadius: 10))
             }
         }
         .padding()
+        .task {
+            await service.businessSearch()
+        }
     }
 }
 
--- CitySights App/DataService.swift
+++ CitySights App/DataService.swift
@@ -6,3 +6,42 @@
 //
 
 import Foundation
+
+struct DataService {
+    
+    let apiKey = Bundle.main.infoDictionary?["API_KEY"] as? String
+    
+    func businessSearch() async {
+        
+        // Check if api key exists
+        guard apiKey != nil else {
+            return
+        }
+        
+        // 1. Create URL
+        if let url = URL(string: "https://api.yelp.com/v3/businesses/search?latitude=35.665517&longitude=139.770398&categories=restaurants&limit=10") {
+            
+            var request = URLRequest(url: url)
+            request.addValue("Bearer \(apiKey!)", forHTTPHeaderField: "Authorization")
+            request.addValue("application/json", forHTTPHeaderField: "accept")
+            
+            do {
+             let (data, response)  = try await URLSession.shared.data(for: request)
+                
+                print(data)
+                print(response)
+            }
+            catch {
+                print(error)
+            }
+            
+        }
+        
+        
+        // 2. Create Request
+       
+        
+        // 3. Send Request
+    }
+    
+}
