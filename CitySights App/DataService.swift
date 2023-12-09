4a312bd6c80277201cf746f9c5467f7392948619
Bobby Kiaie <bobbykiaie@gmail.com>
2023-12-09 13:57:19 -0800

    - Add business models - Add parsing - Return businesses to content view

--- CitySights App/DataService.swift
+++ CitySights App/DataService.swift
@@ -11,25 +11,32 @@ struct DataService {
     
     let apiKey = Bundle.main.infoDictionary?["API_KEY"] as? String
     
-    func businessSearch() async {
+    func businessSearch() async -> [Business]? {
         
         // Check if api key exists
         guard apiKey != nil else {
-            return
+            return [Business]()
         }
         
         // 1. Create URL
         if let url = URL(string: "https://api.yelp.com/v3/businesses/search?latitude=35.665517&longitude=139.770398&categories=restaurants&limit=10") {
             
+            // 2. Create Request
             var request = URLRequest(url: url)
             request.addValue("Bearer \(apiKey!)", forHTTPHeaderField: "Authorization")
             request.addValue("application/json", forHTTPHeaderField: "accept")
             
             do {
+                // 3. Send Request
              let (data, response)  = try await URLSession.shared.data(for: request)
+            
                 
-                print(data)
-                print(response)
+                // 4. Parse the JSON
+                let decoder = JSONDecoder()
+                let result = try decoder.decode(BusinessSearch.self, from: data)
+                print(result.businesses ?? "No Data")
+                return result.businesses
+               
             }
             catch {
                 print(error)
@@ -38,10 +45,9 @@ struct DataService {
         }
         
         
-        // 2. Create Request
        
-        
-        // 3. Send Request
+       
+        return [Business]()
     }
     
 }
--- CitySights App/Models/BusinessSearch.swift
+++ CitySights App/Models/BusinessSearch.swift
@@ -6,3 +6,53 @@
 //
 
 import Foundation
+
+struct BusinessSearch: Decodable {
+    var businesses: [Business]?
+    var region: Region?
+    var total: Int?
+}
+
+struct Business: Decodable {
+    var alias: String?
+    var categories: [Category]?
+    var coordinates: Coordinates?
+    var displayPhone: String?
+    var distance: Double?
+    var id: String?
+    var imageUrl: String?
+    var isClosed: Bool?
+    var location: Location?
+    var name: String?
+    var phone: String?
+    var price: String?
+    var rating: Double?
+    var reviewCount: Int?
+    var transactions: [String]?
+    var url: String?
+}
+
+struct Category: Decodable {
+    var alias: String?
+    var title: String?
+}
+
+struct Coordinates: Decodable {
+    var latitude: Double?
+    var longitude: Double?
+}
+
+struct Location: Decodable {
+    var address1: String?
+    var address2: String?
+    var address3: String?
+    var city: String?
+    var country: String?
+    var displayAddress: [String]?
+    var state: String?
+    var zipCode: String?
+}
+
+struct Region: Decodable {
+    var center: Coordinates?
+}
